0e49d575b6ce35e36f843641a5e30d987a80ea9a
wyywwi <2060034764@qq.com>
2022-11-07 21:19:55 +0800

    add example

---
+++ test_form_1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <!--
+    php代码
+    处理表单测试
+    -->
+    <?php
+        echo "Hello World!", "<br><br>";
+        echo "Hello, $_GET[username]!";
+    ?>
+    </body>
+</html>
